8746613bee3c00c3129de772a1a68e386ee62967
Wei <wei@dappio.xyz>
2022-11-02 22:54:40 +0000

    update IDL

---
+++ target/types/adapter_genopets_staking.ts
@@ -0,0 +1,395 @@
+export type AdapterGenopetsStaking = {
+  "version": "0.1.0",
+  "name": "adapter_genopets_staking",
+  "instructions": [
+    {
+      "name": "stake",
+      "accounts": [
+        {
+          "name": "gatewayAuthority",
+          "isMut": false,
+          "isSigner": true
+        },
+        {
+          "name": "gatewayStateInfo",
+          "isMut": false,
+          "isSigner": false
+        },
+        {
+          "name": "baseProgramId",
+          "isMut": false,
+          "isSigner": false
+        }
+      ],
+      "args": [
+        {
+          "name": "input",
+          "type": "bytes"
+        }
+      ]
+    },
+    {
+      "name": "unstake",
+      "accounts": [
+        {
+          "name": "gatewayAuthority",
+          "isMut": false,
+          "isSigner": true
+        },
+        {
+          "name": "gatewayStateInfo",
+          "isMut": false,
+          "isSigner": false
+        },
+        {
+          "name": "baseProgramId",
+          "isMut": false,
+          "isSigner": false
+        }
+      ],
+      "args": [
+        {
+          "name": "input",
+          "type": "bytes"
+        }
+      ]
+    },
+    {
+      "name": "harvest",
+      "accounts": [
+        {
+          "name": "gatewayAuthority",
+          "isMut": false,
+          "isSigner": true
+        },
+        {
+          "name": "gatewayStateInfo",
+          "isMut": false,
+          "isSigner": false
+        },
+        {
+          "name": "baseProgramId",
+          "isMut": false,
+          "isSigner": false
+        }
+      ],
+      "args": [
+        {
+          "name": "input",
+          "type": "bytes"
+        }
+      ]
+    }
+  ],
+  "types": [
+    {
+      "name": "StakeInputWrapper",
+      "type": {
+        "kind": "struct",
+        "fields": [
+          {
+            "name": "amount",
+            "type": "u64"
+          },
+          {
+            "name": "lockForMonths",
+            "type": "u8"
+          },
+          {
+            "name": "asSgene",
+            "type": "bool"
+          }
+        ]
+      }
+    },
+    {
+      "name": "StakeOutputWrapper",
+      "type": {
+        "kind": "struct",
+        "fields": [
+          {
+            "name": "tokenInAmount",
+            "type": "u64"
+          },
+          {
+            "name": "dummy2",
+            "type": "u64"
+          },
+          {
+            "name": "dummy3",
+            "type": "u64"
+          },
+          {
+            "name": "dummy4",
+            "type": "u64"
+          }
+        ]
+      }
+    },
+    {
+      "name": "UnstakeInputWrapper",
+      "type": {
+        "kind": "struct",
+        "fields": [
+          {
+            "name": "asSgene",
+            "type": "bool"
+          }
+        ]
+      }
+    },
+    {
+      "name": "UnstakeOutputWrapper",
+      "type": {
+        "kind": "struct",
+        "fields": [
+          {
+            "name": "tokenOutAmount",
+            "type": "u64"
+          },
+          {
+            "name": "dummy2",
+            "type": "u64"
+          },
+          {
+            "name": "dummy3",
+            "type": "u64"
+          },
+          {
+            "name": "dummy4",
+            "type": "u64"
+          }
+        ]
+      }
+    },
+    {
+      "name": "HarvestInputWrapper",
+      "type": {
+        "kind": "struct",
+        "fields": []
+      }
+    },
+    {
+      "name": "HarvestOutputWrapper",
+      "type": {
+        "kind": "struct",
+        "fields": [
+          {
+            "name": "dummy1",
+            "type": "u64"
+          },
+          {
+            "name": "dummy2",
+            "type": "u64"
+          },
+          {
+            "name": "dummy3",
+            "type": "u64"
+          },
+          {
+            "name": "dummy4",
+            "type": "u64"
+          }
+        ]
+      }
+    }
+  ]
+};
+
+export const IDL: AdapterGenopetsStaking = {
+  "version": "0.1.0",
+  "name": "adapter_genopets_staking",
+  "instructions": [
+    {
+      "name": "stake",
+      "accounts": [
+        {
+          "name": "gatewayAuthority",
+          "isMut": false,
+          "isSigner": true
+        },
+        {
+          "name": "gatewayStateInfo",
+          "isMut": false,
+          "isSigner": false
+        },
+        {
+          "name": "baseProgramId",
+          "isMut": false,
+          "isSigner": false
+        }
+      ],
+      "args": [
+        {
+          "name": "input",
+          "type": "bytes"
+        }
+      ]
+    },
+    {
+      "name": "unstake",
+      "accounts": [
+        {
+          "name": "gatewayAuthority",
+          "isMut": false,
+          "isSigner": true
+        },
+        {
+          "name": "gatewayStateInfo",
+          "isMut": false,
+          "isSigner": false
+        },
+        {
+          "name": "baseProgramId",
+          "isMut": false,
+          "isSigner": false
+        }
+      ],
+      "args": [
+        {
+          "name": "input",
+          "type": "bytes"
+        }
+      ]
+    },
+    {
+      "name": "harvest",
+      "accounts": [
+        {
+          "name": "gatewayAuthority",
+          "isMut": false,
+          "isSigner": true
+        },
+        {
+          "name": "gatewayStateInfo",
+          "isMut": false,
+          "isSigner": false
+        },
+        {
+          "name": "baseProgramId",
+          "isMut": false,
+          "isSigner": false
+        }
+      ],
+      "args": [
+        {
+          "name": "input",
+          "type": "bytes"
+        }
+      ]
+    }
+  ],
+  "types": [
+    {
+      "name": "StakeInputWrapper",
+      "type": {
+        "kind": "struct",
+        "fields": [
+          {
+            "name": "amount",
+            "type": "u64"
+          },
+          {
+            "name": "lockForMonths",
+            "type": "u8"
+          },
+          {
+            "name": "asSgene",
+            "type": "bool"
+          }
+        ]
+      }
+    },
+    {
+      "name": "StakeOutputWrapper",
+      "type": {
+        "kind": "struct",
+        "fields": [
+          {
+            "name": "tokenInAmount",
+            "type": "u64"
+          },
+          {
+            "name": "dummy2",
+            "type": "u64"
+          },
+          {
+            "name": "dummy3",
+            "type": "u64"
+          },
+          {
+            "name": "dummy4",
+            "type": "u64"
+          }
+        ]
+      }
+    },
+    {
+      "name": "UnstakeInputWrapper",
+      "type": {
+        "kind": "struct",
+        "fields": [
+          {
+            "name": "asSgene",
+            "type": "bool"
+          }
+        ]
+      }
+    },
+    {
+      "name": "UnstakeOutputWrapper",
+      "type": {
+        "kind": "struct",
+        "fields": [
+          {
+            "name": "tokenOutAmount",
+            "type": "u64"
+          },
+          {
+            "name": "dummy2",
+            "type": "u64"
+          },
+          {
+            "name": "dummy3",
+            "type": "u64"
+          },
+          {
+            "name": "dummy4",
+            "type": "u64"
+          }
+        ]
+      }
+    },
+    {
+      "name": "HarvestInputWrapper",
+      "type": {
+        "kind": "struct",
+        "fields": []
+      }
+    },
+    {
+      "name": "HarvestOutputWrapper",
+      "type": {
+        "kind": "struct",
+        "fields": [
+          {
+            "name": "dummy1",
+            "type": "u64"
+          },
+          {
+            "name": "dummy2",
+            "type": "u64"
+          },
+          {
+            "name": "dummy3",
+            "type": "u64"
+          },
+          {
+            "name": "dummy4",
+            "type": "u64"
+          }
+        ]
+      }
+    }
+  ]
+};
