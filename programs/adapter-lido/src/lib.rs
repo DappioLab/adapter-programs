2265f7f0f219ed3734ebd28a630831a153fca4ed
Alvin Philips <alvinphilips257@gmail.com>
2022-11-29 04:16:34 +0530

    Added Output Wrappers and cleaned up Deposit

--- programs/adapter-lido/src/lib.rs
+++ programs/adapter-lido/src/lib.rs
@@ -1,17 +1,16 @@
 use anchor_lang::prelude::*;
 use anchor_lang::solana_program::{
+    stake,
     instruction::{AccountMeta, Instruction},
     program::invoke,
     pubkey::Pubkey,
 };
-use anchor_spl::token::{Mint, TokenAccount};
+use anchor_spl::token::TokenAccount;
 
 declare_id!("ADPTPxbHbEBo9A8E53P2PZnmw3ZYJuwc8ArQQkbJtqhx");
 
 #[program]
 pub mod adapter_lido {
-    use anchor_lang::solana_program::{account_info::next_account_infos, stake};
-
     use super::*;
 
     pub fn deposit<'a, 'b, 'c, 'info>(
@@ -23,11 +22,16 @@ pub mod adapter_lido {
         let input_struct = DepositInputWrapper::deserialize(&mut input_bytes)?;
         
         msg!("Input: {:?}", input_struct);
-     
+
+        let mut recipient_st_sol_account =
+            Account::<TokenAccount>::try_from(&ctx.remaining_accounts[2])?;
+
+        let token_amount = recipient_st_sol_account.amount;
+
         let deposit_accounts = vec![
             AccountMeta::new(ctx.remaining_accounts[0].key(), false),
             AccountMeta::new(ctx.remaining_accounts[1].key(), true),
-            AccountMeta::new(ctx.remaining_accounts[2].key(), false),
+            AccountMeta::new(recipient_st_sol_account.key(), false),
             AccountMeta::new(ctx.remaining_accounts[3].key(), false),
             AccountMeta::new(ctx.remaining_accounts[4].key(), false),
             AccountMeta::new_readonly(ctx.remaining_accounts[5].key(), false),
@@ -35,9 +39,29 @@ pub mod adapter_lido {
             AccountMeta::new_readonly(ctx.remaining_accounts[7].key(), false),  
         ];
 
-        let ix = Instruction { program_id: ctx.accounts.base_program_id.key(), accounts: deposit_accounts, data: input };
+        // Prepend instruction byte to Solido Input
+        let mut data = vec![1u8];
+        data.extend(input);
+
+        let ix = Instruction { program_id: ctx.accounts.base_program_id.key(), accounts: deposit_accounts, data};
         
         invoke(&ix, ctx.remaining_accounts)?;
+
+        recipient_st_sol_account.reload()?;
+
+        let share_amount = recipient_st_sol_account.amount.checked_sub(token_amount).unwrap();
+
+        // Wrap Output
+        let output_struct = DepositOutputWrapper {
+            share_amount,
+            ..Default::default()
+        };
+        let mut output: Vec<u8> = Vec::new();
+        output_struct.serialize(&mut output).unwrap();
+
+        anchor_lang::solana_program::program::set_return_data(&output);
+
+        msg!("Output: {:?}", output_struct);
 
         Ok(())
     }
@@ -46,6 +70,7 @@ pub mod adapter_lido {
         ctx: Context<'a, 'b, 'c, 'info, Action<'info>>,
         input: Vec<u8>,
     ) -> Result<()> {
+        msg!("Entered Withdraw function");
         // Get Input
         let mut input_bytes = &input[..];
         let input_struct = WithdrawInputWrapper::deserialize(&mut input_bytes)?;
@@ -107,8 +132,6 @@ pub struct Action<'info> {
 
 #[derive(AnchorSerialize, AnchorDeserialize, Clone, Debug, Default)]
 pub struct DepositInputWrapper {
-    /// The instruction to deposit. Should always be 1.
-    pub instruction: u8,
     /// Amount to deposit.
     pub amount: u64,
 }
@@ -121,4 +144,51 @@ pub struct WithdrawInputWrapper {
     pub amount: u64,
     /// Index of the Heaviest Validator. Unused in Lido v1.
     pub validator_index: u32,
+}
+
+// OutputWrapper needs to take up all the space of 32 bytes
+#[derive(AnchorSerialize, AnchorDeserialize, Clone, Debug, Default)]
+pub struct DepositOutputWrapper {
+    pub share_amount: u64,
+    pub dummy_2: u64,
+    pub dummy_3: u64,
+    pub dummy_4: u64,
+}
+
+// OutputWrapper needs to take up all the space of 32 bytes
+#[derive(AnchorSerialize, AnchorDeserialize, Clone, Debug, Default)]
+pub struct WithdrawOutputWrapper {
+    pub lp_amount: u64,
+    pub dummy_2: u64,
+    pub dummy_3: u64,
+    pub dummy_4: u64,
+}
+
+pub type DepositOutputTuple = (u64, u64, u64, u64);
+pub type WithdrawOutputTuple = (u64, u64, u64, u64);
+pub type SupplyOutputTuple = (u64, u64, u64, u64);
+pub type UnsupplyOutputTuple = (u64, u64, u64, u64);
+
+impl From<DepositOutputWrapper> for DepositOutputTuple {
+    fn from(result: DepositOutputWrapper) -> DepositOutputTuple {
+        let DepositOutputWrapper {
+            share_amount,
+            dummy_2,
+            dummy_3,
+            dummy_4,
+        } = result;
+        (share_amount, dummy_2, dummy_3, dummy_4)
+    }
+}
+
+impl From<WithdrawOutputWrapper> for WithdrawOutputTuple {
+    fn from(result: WithdrawOutputWrapper) -> WithdrawOutputTuple {
+        let WithdrawOutputWrapper {
+            lp_amount,
+            dummy_2,
+            dummy_3,
+            dummy_4,
+        } = result;
+        (lp_amount, dummy_2, dummy_3, dummy_4)
+    }
 }
