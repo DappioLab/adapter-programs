c70e3f3a8ed68f9504de07e41934b5a0661d559c
emerson <emerson@dappio.xyz>
2022-12-08 16:38:53 +0800

    Remove

--- programs/adapter-tulip/src/common/traits/mod.rs
+++
@@ -1,6 +0,0 @@
-#![allow(clippy::needless_lifetimes)]
-#![allow(clippy::bool_assert_comparison)]
-#![allow(clippy::too_many_arguments)]
-
-pub mod pausable;
-pub mod vault;
--- programs/adapter-tulip/src/farms/unknown.rs
+++
@@ -1,7 +0,0 @@
-//! a catch all farm type indicating an unknown farm / error
-use anchor_lang::prelude::*;
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq, AnchorSerialize, AnchorDeserialize)]
-pub enum Unknown {
-    Uknown,
-}
